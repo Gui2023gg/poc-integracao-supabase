61474b6489863d574dd3b7d0075cce8359668426
Gui2023gg <128082646+Gui2023gg@users.noreply.github.com>
2025-09-03 15:33:50 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const SUPABASE_URL = process.env.SUPABASE_URL ??'';
-const SUPABASE_KEY =  process.env.SUPABASE_URL ??'';
+const SUPABASE_URL = 'https://gmrxubfxmgkeyhzneyqb.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdtcnh1YmZ4bWdrZXloem5leXFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MjM1MTcsImV4cCI6MjA3MjQ5OTUxN30.WybdZ9jLDeEeb3n-Hpn6ZpNbQ9vVhg3MCe9FRTk4NlQ';
  
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
